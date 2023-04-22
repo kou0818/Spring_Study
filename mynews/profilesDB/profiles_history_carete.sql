537ff985f212fd35dea29385fd8c4d21f09b4238
fukuda kodai <fkgo.0818.1993@gmail.com>
2023-04-22 19:55:26 +0900

    2-3課題

--- mynews/profilesDB/profiles_history_carete.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE profiles_histories (
-    id INTEGER DEFAULT nextval('profiles_history_id_seq'::regclass),
-    profiles_id INTEGER NOT NULL,
-    edited_date timestamp(4) with time zone NOT NULL,
-    CONSTRAINT profiles_histories_profiles_id_fk FOREIGN KEY (profiles_id)
-        REFERENCES profiles (id) MATCH SIMPLE
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-);
