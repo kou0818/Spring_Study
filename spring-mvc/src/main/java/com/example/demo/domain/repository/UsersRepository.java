5593f9f340e7f38460929b36420b56cf2a81341e
fukuda kodai <fkgo.0818.1993@gmail.com>
2023-05-07 20:30:01 +0900

    1-6課題修正完了

--- spring-mvc/src/main/java/com/example/demo/domain/repository/UsersRepository.java
+++ spring-mvc/src/main/java/com/example/demo/domain/repository/UsersRepository.java
@@ -1,7 +1,5 @@
 package com.example.demo.domain.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +9,5 @@ import com.example.demo.domain.entity.Users;
 public interface UsersRepository extends JpaRepository<Users, Integer>{
 	
 	//1-6 追記また、Optionalをインポートを実施
-	Optional<Users> findByEmail(String email);
+	boolean existsByEmail(String email);
 }
--- spring-mvc/src/main/java/com/example/demo/domain/service/UsersRegisterService.java
+++ spring-mvc/src/main/java/com/example/demo/domain/service/UsersRegisterService.java
@@ -20,14 +20,15 @@ public class UsersRegisterService {
 	//1-6 存在チェックメソッド定義
 	public boolean isValid(UserForm userForm, BindingResult bindingResult) {
 
-        // E-Mailが重複しているかチェック
-        if (usersRepository.findByEmail(userForm.getEmail()).isPresent()) {
-            bindingResult.rejectValue("email", "", "既に登録されているE-Mailです。");
-            return true;
-        }
+		String  email = userForm.getEmail();
+	// E-Mailが重複しているかチェック
+	    if (usersRepository.existsByEmail(email)) {
+	        bindingResult.rejectValue("email", "", "既に登録されているE-Mailです。");
+	        return true;
+	  }
 
-        return false;
-    }
+	  return false;
+	 }
 
 	public void register(final UserForm userForm) {
 		Users entity = new Users();
@@ -37,4 +38,5 @@ public class UsersRegisterService {
 		entity.setNote(userForm.getNote());
 		usersRepository.save(entity);
 	}
+
 }
